afea36e94a3fba01d977fca9f5e94098dc00d70b
Dionisius Albertus Leonardo Lie <dionisiuss.albertuss@gmail.com>
2023-07-30 13:57:19 +0700

    TASK_5

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+/* eslint-disable */
+<template>
+  <ProductDisplay/>
+</template>
+
+<script>
+import ProductDisplay from './components/ProductDisplay.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDisplay
+  }
+}
+</script>
